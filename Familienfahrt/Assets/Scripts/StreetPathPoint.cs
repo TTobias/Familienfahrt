d022185f9bfc6de882534f2dca25eb844b4a867e
TTobias <58746166+TTobias@users.noreply.github.com>
2020-12-13 05:29:34 +0100

    other stuff

--- Familienfahrt/Assets/Scripts/StreetPathPoint.cs
+++ Familienfahrt/Assets/Scripts/StreetPathPoint.cs
@@ -3,24 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-//[ExecuteInEditMode]
+[ExecuteInEditMode]
 public class StreetPathPoint : MonoBehaviour
 {
     public List<StreetPathPoint> next;
     public float localSpeed = 50f;
 
-    /*
+    
     public bool add = false;
-    public void Update() {
+    public void Update() {/*
         if (add) {
             add = false;
             GameObject tmp = Instantiate(this.gameObject, this.transform.parent);
             next.Add(tmp.GetComponent<StreetPathPoint>());
             tmp.name = this.name;
-        }
+        }*/
 
         for(int i = 0; i< next.Count; i++) {
             Debug.DrawLine(this.transform.position, next[i].transform.position, Color.red);
         }
-    }*/
+    }
 }
