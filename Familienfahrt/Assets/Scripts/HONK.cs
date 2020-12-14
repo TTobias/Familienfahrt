823cb1d6d2712ae298320eaec1cd2cc49f83c5a4
FluffyBearTiger <merscheid89@gmail.com>
2020-12-14 04:11:19 +0100

    SFX

---
+++ Familienfahrt/Assets/Scripts/HONK.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HONK : MonoBehaviour
+{
+    private AudioSource aSource;
+    public AudioClip clip;
+    public float cooldown = 1.5f;
+
+    private float cd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aSource = GetComponent<AudioSource>();
+        cd = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time >= cd)
+        {
+            aSource.PlayOneShot(clip);
+
+            cd = Time.time + cooldown;
+        }
+    }
+}
---
+++ Familienfahrt/Assets/Scripts/SpeedPitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedPitch : MonoBehaviour
+{
+    private AudioSource aSource;
+    public CarMovement cm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cm == null) return;
+
+        aSource.pitch = Mathf.Lerp(.5f, 1.8f, cm.speed / cm.maxSpeed);
+    }
+}
