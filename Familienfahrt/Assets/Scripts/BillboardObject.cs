d2fec5a89201be045a57f2901c04de6647f0cd30
FluffyBearTiger <merscheid89@gmail.com>
2020-12-14 02:28:27 +0100

    InGame City Marker

---
+++ Familienfahrt/Assets/Scripts/BillboardObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardObject : MonoBehaviour
+{
+    private Camera main;
+
+    void Start()
+    {
+        main = Camera.main;
+    }
+
+    void FixedUpdate()
+    {
+        transform.LookAt(main.transform.position, Vector3.up);
+    }
+}
