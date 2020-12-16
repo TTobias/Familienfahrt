891c3d54631fbaaf32c0692388d9375f21a5575b
FluffyBearTiger <merscheid89@gmail.com>
2020-12-16 06:23:36 +0100

    Radio

---
+++ Familienfahrt/Assets/Scripts/Radio.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Radio : MonoBehaviour
+{
+    public AudioClip[] clips;
+    public TMPro.TextMeshProUGUI hoverText;
+    Ray ray;
+    RaycastHit hit;
+    State state = State.None;
+    AudioSource aSource;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aSource = GetComponent<AudioSource>();
+        PlayRandomSong();
+    }
+
+    void FixedUpdate()
+    {
+        switch (state)
+        {
+            case State.None:
+                ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.name == "RadioNextButton")
+                    {
+                        state = State.Over;
+                        hoverText.gameObject.SetActive(true);
+                    }
+                    
+                }
+                break;
+            case State.Over:
+                ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (!(hit.collider.name == "RadioNextButton"))
+                    {
+                        state = State.None;
+                        hoverText.gameObject.SetActive(false);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && state == State.Over)
+        {
+            PlayRandomSong();
+        }
+
+        if (!aSource.isPlaying)
+        {
+            PlayRandomSong();
+        }
+    }
+
+    void PlayRandomSong ()
+    {
+        aSource.Stop();
+        aSource.PlayOneShot(clips[Random.Range(0, clips.Length)]);
+    }
+
+    enum State
+    {
+        None, Over
+    }
+}
+
+
