d4cf9634ab782bd482ff57671f1d18e60fd3a9ad
TTobias <58746166+TTobias@users.noreply.github.com>
2020-12-20 22:50:58 +0100

    added vehicle spawns (AI)

--- Familienfahrt/Assets/Scripts/StreetPathFinder.cs
+++ Familienfahrt/Assets/Scripts/StreetPathFinder.cs
@@ -11,7 +11,25 @@ public class StreetPathFinder : MonoBehaviour
     public bool isTruck = false;
 
     public void Start() {
-        
+        loadRandomCar();
+    }
+
+    public void loadRandomCar() {
+        if (isTruck) {
+            int tmpTruck = (int)Mathf.Floor(Random.Range(0f, 3.98f));
+            string tmpTruck2 = tmpTruck == 0 ? "DHLate" : tmpTruck == 1 ? "Globuli" : tmpTruck == 2 ? "Muggi" : "NukaCola";
+
+            Instantiate(Resources.Load("Prefabs Vehicles/" + tmpTruck2), this.transform.position + new Vector3(0f, 0f, 2.7f), this.transform.rotation, this.transform);
+        }
+        else {
+            int tmpColor = (int)Mathf.Floor(Random.Range(0f, 3.98f));
+            string tmpColor2 = tmpColor == 0 ? "_blue" : tmpColor == 1 ? "_green" : tmpColor == 2 ? "_red" : "_white";
+            int tmpCar = (int)Mathf.Floor(Random.Range(1f, 5.98f));
+            string tmpCar2 = tmpCar.ToString();
+            
+            Instantiate(Resources.Load("Prefabs Vehicles/Car0" + tmpCar2 + tmpColor2), this.transform.position, this.transform.rotation, this.transform);
+        }
+
     }
 
 
